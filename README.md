bded3cb2a2d89aa9a56501ab7bbc23845bbc5b25
A Vaartjes <aaron.va@gmail.com>
2016-10-10 03:17:17 -0400

    Neatened up code and made initial README

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # TODO
 
 1. Make good README
+
+# Basic Usage
+
+node mutli-get.js <url> [filename=out] [num=4] [size=1048576]
+
+num and size must be positive non-zero integers
+
+To avoid sending too many requests to the server, num must be below 1000.
+
+# Files
+
+## multi-get.js
+
+The main application; see above for usage.
+
+## test.sh
+
+A small bash script useful for manual testing. Tiny and not needed for
+running.
--- multi-get.js
+++ multi-get.js
@@ -1,13 +1,5 @@
-//const urlStr = "http://dist.pravala.com/coding/multiGet-example.zip";
-//const urlStr = "http://www.textfiles.com/rpg/bagofwonder.txt";
-
 var http = require('http');
 var fs = require('fs');
-
-function invalidUsage() {
-    process.stdout.write("Usage: node mutli-get.js <url> [filename=out] [number=4] [size=1048576]\n");
-    process.exit();
-}
 
 const urlStr = process.argv[2];
 const outputFile = process.argv[3] || "out";
@@ -15,10 +7,21 @@ const outputFile = process.argv[3] || "out";
 const numChunks = process.argv[4] || 4;
 const chunkSize = process.argv[5] || 1048576;
 
-if (!urlStr || urlStr === "--help" || urlStr === "-h")
-    invalidUsage();
+var successChunks = 0;
+var excessChunks = 0;
 
 var fd;
+
+// Verify url was given correctly
+if (!urlStr || urlStr === "--help" || urlStr === "-h")
+    invalidUsageHandler();
+
+// Verify numeric inputs are valid
+if (isNaN(numChunks) || numChunks <= 0 || numChunks >= 1000 || isNaN(chunkSize) || chunkSize <= 0 || )
+    invalidUsageHandler();
+
+const url = require('url').parse(urlStr);
+
 fs.open(outputFile, "w", (err, fd0) => {
     // TODO consider very unlikely edge case where this doesn't load in time
     if (err) process.stderr.write(err + '\n');
@@ -26,14 +29,16 @@ fs.open(outputFile, "w", (err, fd0) => {
     process.stdout.write("Opened file for writing\n");
 });
 
-const url = require('url').parse(urlStr);
+for (var i = 0; i < numChunks; i++) {
+    requestPart(i);
+}
 
-var excessChunks = 0;
-var successChunks = 0;
+process.on('exit', exitHandler);
 
-function requestPart (i) {
+function requestPart (i, delay) {
     var offset = i * chunkSize;
     var last = (i + 1) * chunkSize - 1;
+
     http.get({
         hostname: url.hostname,
         path: url.path,
@@ -41,9 +46,10 @@ function requestPart (i) {
             'Range':`bytes=${offset}-${last}`
         }
     }, (res) => {
-        if (res.statusCode === 206) { // Partial content status code
+        if (res.statusCode === 206) { // "Partial Content" status code
             successChunks++;
             process.stdout.write(`Downloaded ${successChunks + excessChunks}/${numChunks} chunks \r`);
+
             var data = new Buffer(chunkSize);
             var innerChunkOffset = 0;
 
@@ -52,7 +58,7 @@ function requestPart (i) {
                 fs.writeSync(fd, chunk, 0, chunk.length, offset + innerChunkOffset);
                 innerChunkOffset += chunk.length; 
             });
-        } else if (res.statusCode === 416) {
+        } else if (res.statusCode === 416) { // "Requested Range Not Satisfiable" Status Code
             excessChunks++;
             process.stdout.write(`Downloaded ${successChunks + excessChunks}/${numChunks} chunks \r`);
         } else {
@@ -63,16 +69,16 @@ function requestPart (i) {
     });
 }
 
-for (var i = 0; i < numChunks; i++) {
-    requestPart(i);
+function invalidUsageHandler() {
+    process.stdout.write("Usage: node mutli-get.js <url> [filename=out] [num=4] [size=1048576]\n");
+    process.stdout.write("num and size must be positive non-zero integers\n");
+    process.stdout.write("To avoid sending too many requests to the server, num must be below 1000\n");
+    process.exit();
 }
 
 function exitHandler() {
-    process.stdout.write('\n');
-    process.stdout.write(`There were ${excessChunks} excess chunks.\n`);
+    process.stdout.write(`\nThere were ${excessChunks} excess chunks.\n`);
     
     if (fd)
         fs.closeSync(fd);
 }
-
-process.on('exit', exitHandler);
